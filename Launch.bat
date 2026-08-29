276da58a0fbf8479e14d918003166b082ce23870
ZacharyGantner <zachgantner@gmail.com>
2026-08-28 23:03:37 -0400

    Removed the .bat file because it is personalized to my machine

--- Launch.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-g++ -std=c++17 src/Minesweeper.cpp -I sfml/include -L sfml/lib -lsfml-graphics -lsfml-window -lsfml-system -o Minesweeper.exe
-
-set PATH=%CD%\sfml\bin;%PATH%
-
-cmd /k Minesweeper.exe
-pause
